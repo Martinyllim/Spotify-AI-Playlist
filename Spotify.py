e52c02600e6335c54bd991b2d45629e4b9b5f165
Martin Üllim <martinullim@gmail.com>
2023-12-12 19:50:23 +0200

    Next level UIs

--- Spotify.py
+++ Spotify.py
@@ -8,6 +8,8 @@ import socketserver
 import json
 import openai
 from dotenv import load_dotenv
+import tkinter as tk
+from tkinter import messagebox
 
 # Load environment variables from .env file
 load_dotenv()
@@ -136,6 +138,101 @@ def search_spotify_track(track_name, access_token):
         return None
     else:
         return tracks[0]['uri']
+
+def generate_playlist_name(theme):
+    openai.api_key = OPENAI_API_KEY
+
+    prompt = f"Generate a creative playlist name for a playlist with songs about {theme}."
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=100  # Adjust as needed
+    )
+    playlist_name = response.choices[0].text.strip()
+    return playlist_name
+
+# Modify your main function
+def main():
+    code = get_auth_code()
+    token_response = get_tokens(code)
+    access_token = token_response['access_token']
+
+    user_id = get_user_id(access_token)
+
+    # Use the new function to generate a playlist name based on a theme
+    theme = "Estonian songs"  # Or any other theme you'd like
+    playlist_name = generate_playlist_name(theme)
+
+    # Now create the playlist with the generated name
+    new_playlist_id = create_playlist(user_id, playlist_name, access_token)
+
+    if not new_playlist_id:
+        print("Failed to create new playlist")
+        return
+
+def on_generate_button_clicked():
+    prompt = prompt_entry.get()  # Get the prompt from the input field
+
+    # Generate playlist name and create playlist
+    playlist_name = generate_playlist_name(prompt)
+    playlist_name_label.config(text=f"Playlist Name: {playlist_name}")
+    new_playlist_id = create_playlist(user_id, playlist_name, access_token)
+
+    if not new_playlist_id:
+        messagebox.showerror("Error", "Failed to create new playlist")
+        return
+
+    # Get playlist suggestions and add them to the playlist
+    suggested_tracks = get_playlist_suggestions(prompt)
+    track_names = suggested_tracks.splitlines()
+
+    track_uris = [search_spotify_track(track, access_token) for track in track_names if track]
+    track_uris = [uri for uri in track_uris if uri]
+
+    response = add_tracks_to_playlist(new_playlist_id, track_uris, access_token)
+
+    # Display success message or error
+    if response:
+        messagebox.showinfo("Success", "Playlist created successfully!")
+    else:
+        messagebox.showerror("Error", "Failed to add tracks to the playlist")
+
+# Main function to set up the UI
+def setup_ui():
+    global prompt_entry
+    global playlist_name_label
+    global user_id
+    global access_token
+
+    # Authenticate and get access token
+    code = get_auth_code()
+    token_response = get_tokens(code)
+    access_token = token_response['access_token']
+    user_id = get_user_id(access_token)
+
+    # Create the main window
+    root = tk.Tk()
+    root.title("Spotify Playlist Generator")
+
+    # Create a label and input field for the prompt
+    tk.Label(root, text="Enter your prompt:").pack()
+    prompt_entry = tk.Entry(root, width=50)
+    prompt_entry.pack()
+
+    # Create a button to generate the playlist
+    generate_button = tk.Button(root, text="Generate Playlist", command=on_generate_button_clicked)
+    generate_button.pack()
+
+    # Label to display the generated playlist name
+    playlist_name_label = tk.Label(root, text="Playlist Name:")
+    playlist_name_label.pack()
+
+    # Run the UI loop
+    root.mainloop()
+
+# Call the UI setup function if the script is run directly
+if __name__ == "__main__":
+    setup_ui()
 
 def main():
     code = get_auth_code()
